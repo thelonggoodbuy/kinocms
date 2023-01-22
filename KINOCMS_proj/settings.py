aa329f37577d93b7555465ae68b88df10407899f
thelonggoodbuy <thelonggoodbuy1991@gmail.com>
2023-01-22 17:23:27 +0200

    debuging bug with POST requests v14

--- KINOCMS_proj/settings.py
+++ KINOCMS_proj/settings.py
@@ -63,7 +63,7 @@ INSTALLED_APPS = [
     'tempus_dominus',
     'django_cleanup.apps.CleanupConfig',
     'django_user_agents',
-    # 'corsheaders',
+    'corsheaders',
 
     'pages.apps.PagesConfig',
     'users.apps.UsersConfig',
@@ -75,7 +75,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
-    # 'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -133,7 +133,10 @@ DATABASES = {
 # SESSION_COOKIE_SECURE = True
 
 # CSRF_COOKIE_SAMESITE = 'Strict'
+CORS_ALLOWED_ORIGINS = ['http://24.199.118.134:1337',]
 CSRF_TRUSTED_ORIGINS = ['http://24.199.118.134:1337',]
+
+
 
 # CORS_ORIGIN_ALLOW_ALL = False
 # CORS_ALLOW_ALL_ORIGINS = True
