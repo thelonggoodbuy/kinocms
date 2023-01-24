f512ff1a9a04a8f24e82038a274046c10ade6142
thelonggoodbuy <thelonggoodbuy1991@gmail.com>
2023-01-25 00:32:38 +0200

    debugging init scripts v23

--- cinema/management/commands/shows_initial_script.py
+++ cinema/management/commands/shows_initial_script.py
@@ -10,28 +10,33 @@ import random
 class Command(BaseCommand):
     help = 'Створення показів на найближчий тиждень'
     
-    shows_dates = [] 
-    for day_of_show in range(1, 7):
-        date = datetime.now().date() + timedelta(days=day_of_show)
-        shows_dates.append(date)
-        
+    test_shows = Show.objects.all()
+    if test_shows.exists():
+        print('\nCMS have shows. Shows initialization is aborted. \n')
+    else: 
 
-    shows_time = []
-    for seanse_time in range(1, 18, 3):
-        seanse = datetime.combine(datetime.now(),time(8, 0)) + timedelta(hours=seanse_time)
-        shows_time.append(seanse.time())
+        shows_dates = [] 
+        for day_of_show in range(1, 7):
+            date = datetime.now().date() + timedelta(days=day_of_show)
+            shows_dates.append(date)
+            
+        shows_time = []
+        for seanse_time in range(1, 18, 3):
+            seanse = datetime.combine(datetime.now(),time(8, 0)) + timedelta(hours=seanse_time)
+            shows_time.append(seanse.time())
 
-    all_movies = Movie.objects.all()
+        all_movies = Movie.objects.all()
 
-    all_cinema_halls = CinemaHall.objects.all()
+        all_cinema_halls = CinemaHall.objects.all()
 
-    def handle(self, *args, **kwargs):
-        for show_date in self.shows_dates:
-            for show_time in self.shows_time:
-                Show.objects.get_or_create(
-                    movie = (random.choice(self.all_movies)),
-                    cinema_hall = (random.choice(self.all_cinema_halls)),
-                    date_show = show_date,
-                    time_show = show_time,
-                    cost = 100
-                )
+        def handle(self, *args, **kwargs):
+            for show_date in self.shows_dates:
+                for show_time in self.shows_time:
+                    Show.objects.get_or_create(
+                        movie = (random.choice(self.all_movies)),
+                        cinema_hall = (random.choice(self.all_cinema_halls)),
+                        date_show = show_date,
+                        time_show = show_time,
+                        cost = 100
+                    )
+        print('\nInit shows have created.\n')
